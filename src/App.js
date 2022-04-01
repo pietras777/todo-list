34e4e578d3b6d090de3772ea9b76737319e83fb4
Piotr Wendt <wendtpiotr333@gmail.com>
2022-04-01 21:15:51 +0200

    Created basic logic of an app, added basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+import Todo from "./components/Todo";
+
 const App = () => {
-  return <div>hello world</div>;
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  useEffect(() => {
+    console.log("todos ===", todos);
+  }, [todos]);
+
+  const handleTodoValue = (event) => {
+    setTodoValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (todoValue.length > 0) {
+      setTodos((prevArray) => [...prevArray, todoValue]);
+    } else return;
+  };
+
+  return (
+    <>
+      <h1>Todo list</h1>
+      <input type="text" onChange={(ev) => handleTodoValue(ev)} />
+      <button onClick={handleAddTodo}>Add todo</button>
+      {todos.map((el) => (
+        <Todo key={el} todo={el} />
+      ))}
+    </>
+  );
 };
 
 export default App;
